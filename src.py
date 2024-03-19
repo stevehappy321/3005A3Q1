4fbc3117c09e63864fa5547ee1f2b22215766b2c
stevehappy321 <90816857+stevehappy321@users.noreply.github.com>
2024-03-18 23:42:20 -0400

    Update src.py

--- src.py
+++ src.py
@@ -8,6 +8,8 @@ conn = psycopg2.connect(
     port="5432")
 conn.autocommit = True
 
+#add new student to table
+#inputs: first name, last name, email, enrollment date (yyyy-mm-dd)
 def addStudent(first_name, last_name, email, enrollment_date):
     cursor = conn.cursor()
     cursor.execute(
@@ -15,6 +17,8 @@ def addStudent(first_name, last_name, email, enrollment_date):
         f"VALUES ('{first_name}', '{last_name}', '{email}', '{enrollment_date}')"
         )
 
+#change student email
+#inputs: student id, new email address
 def updateStudentEmail(student_id, new_email):
     cursor = conn.cursor()
     cursor.execute(
@@ -23,6 +27,8 @@ def updateStudentEmail(student_id, new_email):
         f"WHERE student_id = {student_id}"
         )
 
+#delete student from table
+#inputs: student id
 def deleteStudent(student_id):
     cursor = conn.cursor()
     cursor.execute(
@@ -30,6 +36,7 @@ def deleteStudent(student_id):
         f"WHERE student_id = {student_id}"
         )
 
+#print all students as array
 def getAllStudents():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Students")
